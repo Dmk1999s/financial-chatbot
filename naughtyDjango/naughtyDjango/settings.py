102ee236cfaad6198b0ffd4ed8a2fd1ea9e5a656
hyoin <hyoinyang02@gmail.com>
2025-06-24 17:18:14 +0900

    [REFACTOR] 장고 인증 삭제

--- naughtyDjango/naughtyDjango/settings.py
+++ naughtyDjango/naughtyDjango/settings.py
@@ -58,7 +58,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
 }
 
