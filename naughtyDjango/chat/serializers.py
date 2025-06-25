eb0b1b83a56be30eae83e3b1274a6ee40a47c4cc
hyoin <hyoinyang02@gmail.com>
2025-06-25 14:09:40 +0900

    [REFACTOR] 스웨거 문서 수정

--- naughtyDjango/chat/serializers.py
+++ naughtyDjango/chat/serializers.py
@@ -4,3 +4,22 @@ class ChatRequestSerializer(serializers.Serializer):
     id = serializers.CharField(required=False, allow_blank=True, allow_null=True)
     session_id = serializers.CharField(required=False, allow_blank=True, allow_null=True)
     message = serializers.CharField(required=True)
+
+class InvestmentProfileSerializer(serializers.Serializer):
+    risk_tolerance = serializers.CharField()
+    age = serializers.IntegerField()
+    income_stability = serializers.CharField()
+    income_sources = serializers.CharField()
+    monthly_income = serializers.FloatField()
+    investment_horizon = serializers.CharField()
+    expected_return = serializers.CharField()
+    expected_loss = serializers.CharField()
+    investment_purpose = serializers.CharField()
+
+class SaveInvestmentProfileRequestSerializer(serializers.Serializer):
+    session_id = serializers.CharField()
+    user_id = serializers.CharField()
+    investment_profile = InvestmentProfileSerializer()
+
+class RecommendProductRequestSerializer(serializers.Serializer):
+    query = serializers.CharField()
