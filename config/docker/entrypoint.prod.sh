d222fdbabb1e3a9f2705e5a01019cfc9bc2a8a3a
hyoin <hyoinyang02@gmail.com>
2025-07-16 21:03:54 +0900

    [CI] 테스트를 위한 정보 삭제

--- config/docker/entrypoint.prod.sh
+++ config/docker/entrypoint.prod.sh
@@ -3,19 +3,6 @@
 # 진입점 경로 수정
 cd /app/naughtyDjango
 
-
-echo "==== [WORKDIR] 현재 작업 디렉토리 ===="
-pwd
-
-echo "==== [/app] 디렉토리 구조 ===="
-ls -al /app
-
-echo "==== [/app/naughtyDjango] 디렉토리 구조 ===="
-ls -al /app/naughtyDjango
-
-echo "==== Python 패키지 경로(sys.path) ===="
-python -c "import sys; print(sys.path)"
-
 # 마이그레이션, collectstatic 등 필요한 커맨드
 python manage.py migrate
 python manage.py collectstatic --noinput
