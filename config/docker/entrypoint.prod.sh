fb98c04085c3458e8bb0b6e4d1576f4384838cfe
hyoin <hyoinyang02@gmail.com>
2025-07-16 16:23:30 +0900

    [CI] gunicorn 경로 수정 2

--- config/docker/entrypoint.prod.sh
+++ config/docker/entrypoint.prod.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # 진입점 경로 수정
-cd /app/naughtyDjango
+cd /app
 
 # 마이그레이션, collectstatic 등 필요한 커맨드
 python manage.py migrate
 python manage.py collectstatic --noinput
 
 # gunicorn 실행
-exec gunicorn --chdir /app/naughtyDjango naughtyDjango.main.wsgi:application --bind 0.0.0.0:8000
+exec gunicorn naughtyDjango.main.wsgi:application --bind 0.0.0.0:8000
