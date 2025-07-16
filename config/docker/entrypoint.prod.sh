bce973d96b92ba61967e47551dd6fc388e1baa9a
hyoin <hyoinyang02@gmail.com>
2025-07-16 16:46:30 +0900

    [CI] 디버깅 메시지 추가

--- config/docker/entrypoint.prod.sh
+++ config/docker/entrypoint.prod.sh
@@ -3,9 +3,27 @@
 # 진입점 경로 수정
 cd /app
 
+
+echo "==== [WORKDIR] 현재 작업 디렉토리 ===="
+pwd
+
+echo "==== [/app] 디렉토리 구조 ===="
+ls -al /app
+
+echo "==== [/app/naughtyDjango] 디렉토리 구조 ===="
+ls -al /app/naughtyDjango
+
+echo "==== Python 패키지 경로(sys.path) ===="
+python -c "import sys; print(sys.path)"
+
+echo "==== 파이썬 주요 패키지 현황(pip list) ===="
+pip list
+
 # 마이그레이션, collectstatic 등 필요한 커맨드
 python manage.py migrate
 python manage.py collectstatic --noinput
 
+export PYTHONPATH=/app
+
 # gunicorn 실행
-exec gunicorn --pythonpath /app main.wsgi:application --bind 0.0.0.0:8000
+exec gunicorn naughtyDjango.main.wsgi:application --bind 0.0.0.0:8000
