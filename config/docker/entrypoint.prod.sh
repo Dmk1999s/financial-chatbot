c547819b24bdd3af8c7e22167eebc9c04ddf6d1f
hyoin <hyoinyang02@gmail.com>
2025-07-16 16:12:43 +0900

    [CI] gunicorn에 옵션 추가

--- config/docker/entrypoint.prod.sh
+++ config/docker/entrypoint.prod.sh
@@ -3,11 +3,9 @@
 # 진입점 경로 수정
 cd /home/app/naughtyDjango
 
-export PYTHONPATH=$(pwd)
-
 # 마이그레이션, collectstatic 등 필요한 커맨드
 python manage.py migrate
 python manage.py collectstatic --noinput
 
 # gunicorn 실행
-exec gunicorn main.wsgi:application --bind 0.0.0.0:8000
+exec gunicorn --chdir /home/app/naughtyDjango naughtyDjango.main.wsgi:application --bind 0.0.0.0:8000
