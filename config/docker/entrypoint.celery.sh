7a1df8d9194ffe6d46b589761f6fa79d1c72ce95
Dmk1999s <loik1235@gmail.com>
2025-09-20 20:39:39 +0900

    celery 메모리 확장

--- config/docker/entrypoint.celery.sh
+++ config/docker/entrypoint.celery.sh
@@ -12,4 +12,11 @@ done
 echo "Redis is ready!"
 
 # Start Celery worker
-exec celery -A main worker -l info --concurrency=2
+#exec celery -A main worker -l info --concurrency=2
+
+# Celery worker를 메모리 친화적으로 실행
+exec celery -A main worker -l info \
+  --concurrency=1 \
+  --prefetch-multiplier=1 \
+  --max-tasks-per-child=1 \
+  --max-memory-per-child=350000
